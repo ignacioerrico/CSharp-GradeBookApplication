435639d7ba25908085b5c551278d9661c555bc7d
Ignacio Errico <ignacio.errico@gmail.com>
2018-06-04 00:22:41 +0200

    Implement rank-grading for the RankedGradeBook
    - Make sure there are least five students
    - Calculate the rank grade based on the position of the input grade in the list of average grade of each student when sorted in descending order

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System.Linq;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,38 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Get the list of the average grade of each student, in descending order.
+            var orderedAverageGrades = Students.Select(s => s.AverageGrade).OrderByDescending(g => g);
+
+            // Where is the input grade placed among the existing grades?
+            int index = 0;
+            foreach (var grade in orderedAverageGrades)
+            {
+                if (averageGrade >= grade)
+                {
+                    break;
+                }
+
+                ++index;
+            }
+
+            var rank = (double)index / Students.Count;
+
+            if (rank < .2) return 'A';
+            else if(rank < .4) return 'B';
+            else if(rank < .6) return 'C';
+            else if(rank < .8) return 'D';
+
+            return 'F';
         }
     }
 }
